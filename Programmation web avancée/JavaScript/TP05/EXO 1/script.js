40e21109155f7c7fbbf6d9c286cb6bce1b296d74
Mohamed Wassim Blilita <94299660+WassimBlilita7@users.noreply.github.com>
2023-11-20 20:45:24 +0100

    Update script.js

--- Programmation web avancée/JavaScript/TP05/EXO 1/script.js
+++ Programmation web avancée/JavaScript/TP05/EXO 1/script.js
@@ -1,6 +1,6 @@
 function showWelcome(){
     var nameInput = document.getElementById("name");
-    var welcomeMessage = document.getElementById("welcome-message");
+    var welcomeMessage = document.querySelector("#welcome-message");
 
     var name = nameInput.value;
     welcomeMessage.textContent = `Welcome to the site ${name}`;
